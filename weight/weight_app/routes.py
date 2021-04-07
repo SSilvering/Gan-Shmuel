92d40d06fe199090f4ce820c8e9bf9ae2d8c3c7e
fadi <fadi1707@outlook.com>
2021-04-07 23:01:31 +0300

    healthcheck refined

--- weight/weight_app/routes.py
+++ weight/weight_app/routes.py
@@ -1,3 +1,4 @@
+
 from weight_app import weight_app, requests
 
 
@@ -8,11 +9,16 @@ def index():
     return "Hello, World!"
 
 
-@weight_app.route('/health')
-def health_check():
-    req = requests.get("http://localhost:5000/weight")
-    print(req.status_code)
-    r = req.status_code
-    return str(r)
-    
+@weight_app.route('/health', methods=['GET', 'POST'])
+def health_check(api='<item>'):
+    #a tuple of the all the apis that will be used in the project 
+    api_tuple = ("index","batch-weight","unknown","item","session","post","weight")
+    for item in api_tuple:
+        uri = f"http://localhost:5000/{item}"
+        req = requests.get(uri)
+        print(req.status_code)
+        if req.status_code < 200 or req.status_code > 299:
+            res = f"APP Status is {req.status_code}"
+            return res
+    return f"APP status is {req.status_code}"
     

54b2d2313782369700d8a8a7661ee84a17e4886d
Tomer <chaim.tomer@gmail.com>
2021-04-08 20:27:01 +0300

    fixed some issues

--- billing/app/setup/settings.py
+++ billing/app/setup/settings.py
@@ -1,9 +1,7 @@
 import os
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
-<<<<<<< HEAD
+
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flask:flask@db/db'
-=======
-SQLALCHEMY_DATABASE_URI = 'sqlite:///billing.db'
->>>>>>> refs/remotes/origin/billing
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///billing.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False

3ba296ca4018bddd4da436d94afdc0cb4f5689ce
Daniel Sukharev <virusnegadai@gmail.com>
2021-04-08 03:54:29 -0700

    check branch name

--- devops/CI/run.py
+++ devops/CI/run.py
@@ -3,6 +3,10 @@ import os
 from flask import Flask, request, json, Response
 from git import Repo
 
+
+
+branches = ['billing', 'weight', 'devops']
+
 app = Flask(__name__)
 
 
@@ -17,19 +21,26 @@ def hook():
         data = json.dumps(request.json)
         print(data)
 
-
-
-        # repo = Repo(current_app.config.get('REPO_PATH'))
-        repo = Repo('https://github.com/SSilvering/Gan-Shmuel.git') ## CHANGE IT        
-        origin = repo.remotes.origin
-        origin.pull('--rebase')
-        return Response(status=200)
+        if check_push(data):
+            # repo = Repo(current_app.config.get('REPO_PATH'))
+            repo = Repo('https://github.com/SSilvering/Gan-Shmuel.git') ## CHANGE IT        
+            origin = repo.remotes.origin
+            origin.pull('--rebase')
+            return Response(status=200)
 
 
 @app.route('/health', methods=['GET'])
 def health():
     print("Health Check")
     return Response(status=200)
+
+
+def check_push(data):
+    branch = data['ref'].split('/')[2]
+    if branch in branches:
+        return True
+    else:
+        return False
 
 
 if __name__ == '__main__':

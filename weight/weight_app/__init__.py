052d31814a2e1138c6ed2c6c4238d001ec79901c
Younatan <younatan.huber@gmail.com>
2021-04-08 13:17:13 +0300

    intialization + sql handler

--- weight/weight_app/__init__.py
+++ weight/weight_app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask, redirect, request, session
 import requests
+import mysql.connector
+from mysql.connector import Error
 
 weight_app = Flask(__name__)
 
 from weight_app import routes
-
--- weight/weight_app/routes.py
+++ weight/weight_app/routes.py
@@ -1,7 +1,9 @@
 
 from weight_app import weight_app, requests
-
-
+from time import gmtime, strftime
+from flask import request
+import mysql.connector
+from get_weight import get_weight
 
 @weight_app.route('/')
 @weight_app.route('/index')
@@ -21,4 +23,14 @@ def health_check():
             res = f"APP Status is {req.status_code}"
             return res
     return f"APP status is {req.status_code}"
+@weight_app.route('/weight', methods=['GET'])
+def get_weight_startup():
+    #time_format yyyymmddhhmmss
+    currenttime = strftime("%Y%m%d%H%M%S", gmtime())
+    start_of_day = strftime("%Y%m%d000000", gmtime())
+    from_time = request.args.get('from', default = start_of_day, type = str)
+    to_time = request.args.get('to', default = currenttime, type = str)
+    filter_type = request.args.get('filter', default = '*', type = str)
+    db_name = db #needs assignment
+    get_weight(from_time,to_time,filter_type,db_name)
     

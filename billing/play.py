7a3fdbca1a7e2ed625980c2fde127105e120ad04
Tomer <chaim.tomer@gmail.com>
2021-04-08 23:02:19 +0300

    fixed the docker-compose

--- billing/play.py
+++ billing/play.py
@@ -1,5 +1,27 @@
-import requests
-
-res = requests.post('http://127.0.0.1:5000/truck', params={'provider_name': 'tomer', 'truck_id': '1571'})
-
-print(res.text)
+# import subprocess
+#
+#
+# # def subprocess_cmd(command):
+# #     process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+# #     proc_stdout = process.communicate()[0].strip()
+# #     print(proc_stdout)
+# #
+# #
+# # subprocess_cmd('mkdir hello && cd hello && echo hello > file.txt')
+#
+#
+# def up_container(branch):
+#     bashCommands = [f'mkdir -p /home/ec2-user/tmp/build_{branch}',
+#                     f'cd /home/ec2-user/tmp/build_{branch}',
+#                     'git clone https://github.com/SSilvering/Gan-Shmuel.git',
+#                     f'cd /home/ec2-user/tmp/build_{branch}/{branch}',
+#                     f'git checkout {branch}',
+#                     'docker-compose up --build --force-recreate']
+#
+#     command = ' && '.join(x for x in bashCommands)
+#     print(command)
+#     print("+++++++++++++++++++++++++++++++++++++++++++++++")
+#
+#
+# up_container('billing')
+# "mkdir -p /home/ec2-user/tmp/build_tomer && cd /home/ec2-user/tmp/build_tomer && git clone https://github.com/SSilvering/Gan-Shmuel.git && cd /home/ec2-user/tmp/build_tomer/tomer && git checkout tomer && docker-compose up --build --force-recreate"

140b70de6fafea72195c1bb9cd493a251b833391
Younatan <younatan.huber@gmail.com>
2021-04-08 19:44:18 +0300

    -mysql_weight +weight_testing_db

--- weight/weight_app/GETweight.py
+++ weight/weight_app/GETweight.py
@@ -7,26 +7,21 @@ class session:
     bruto = 0
     neto = 0
     produce = "product_name"
-    container = ""
-    def __init__(self, id, direction, bruto, neto, product_name, container): 
+    #container = ""
+    def __init__(self, id, direction, bruto, neto, product_name): #, container):
         self.id = id
         self.direction = direction
         self.bruto = bruto
         self.neto = neto
         self.product_name = product_name
-        self.container = container
+        #self.container = container
 
 
 def GETweight(from_time,to_time,filter_type,db_name):
     #time_format yyyymmddhhmmss
-    mydb = mysql.connector.connect(
-        host="db",
-        user="root",
-        password="123",
-        database=db_name
-    )
+    mydb = mysql.connector.connect(host="db", user="root", password="123", database=db_name)
     session_list =[]
-    sql_string = "SELECT id FROM sessions WHERE date BETWEEN %d AND %d" % (from_time,to_time)
+    sql_string = "SELECT id FROM sessions WHERE date BETWEEN %d AND %d;" % (from_time,to_time)
     if filter_type=="in" or filter_type=="out":
         sql_string+=" AND direction='%s'"
     mycursor = mydb.cursor()
@@ -34,16 +29,17 @@ def GETweight(from_time,to_time,filter_type,db_name):
     myresults = mycursor.fetchall()
     for result in myresults:
         id = result
-        mycursor.execute("SELECT bruto FROM sessions WHERE id='%d'" % result)
+        mycursor.execute("SELECT bruto FROM sessions WHERE id='%d';" % result)
         bruto = mycursor.fetchone()
-        mycursor.execute("SELECT neto FROM sessions WHERE id='%d'" % result)
+        mycursor.execute("SELECT neto FROM sessions WHERE id='%d';" % result)
         neto = mycursor.fetchone()
-        mycursor.execute("SELECT products_id FROM sessions WHERE id='%d'" % result)
+        mycursor.execute("SELECT products_id FROM sessions WHERE id='%d';" % result)
         product_id = mycursor.fetchone()
-        mycursor.execute("SELECT product_name FROM products WHERE id='%d'" % product_id)
+        mycursor.execute("SELECT product_name FROM products WHERE id='%d';" % product_id)
         produce = mycursor.fetchone()
-        mycursor.execute("SELECT containers_id FROM containers_has_sessions WHERE sessions_id='%s'" % result)
-        container = mycursor.fetchone()
-        session_list.append( session(id, filter_type, bruto, neto, produce, container) )
+        #This returns an empty set!
+        ##mycursor.execute("SELECT containers_id FROM containers_has_sessions WHERE sessions_id='%s';" % result)
+        ##container = mycursor.fetchone()
+        session_list.append( session(id, filter_type, bruto, neto, produce) )#, container) )
 
-    return "{ \"id\": <id>, \"direction\": in/out/none, \"bruto\": <int>, //in kg \"neto\": <int> or \"na\", \"produce\": <str>, \"container\": <str> }"
+    return "{ \"id\": <id>, \"direction\": in/out/none, \"bruto\": <int>, //in kg \"neto\": <int> or \"na\", \"produce\": <str> }"#, \"container\": <str> }"

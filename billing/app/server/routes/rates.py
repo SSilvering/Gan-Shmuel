65e890c277515dab073d4f8ff99fcd94c096a949
awny shaheen <awny.shaheen@gmail.com>
2021-04-10 16:16:39 +0300

    More fixes and checks for rates and provides folders, also now parameters can be sent to these routes by both ways, form and json.

--- billing/app/server/routes/rates.py
+++ billing/app/server/routes/rates.py
@@ -1,42 +1,49 @@
 # ------- 3rd party imports -------
 from flask import Blueprint, request
-from flask import Blueprint, request,jsonify
+from flask import Blueprint, request, jsonify
 import openpyxl
 import os
 from pathlib import Path
 from app.server.db.helper import helper
 from app.server.db.models import *
+
 # ------- local imports -------
 rates_blueprint = Blueprint('rates_blueprint', __name__)
 
 
-@rates_blueprint.route("/rates", methods=['GET', 'POST'])
-def rates():
-    if request.method == 'GET':
-        pass
+@rates_blueprint.route("/rates", methods=['GET'])
+def download_rates_to_xml():
+    return "OK\n", 200
 
-    elif request.method == 'POST':
-        print(os.getcwd())
-        xlsx_file = Path(os.getcwd()+'/app/static/rates.xlsx')
+
+@rates_blueprint.route("/rates", methods=['POST'])
+def upload_xml_data():
+    data = (request.form and request.form["file"]) or (request.json and request.json.get("file"))
+
+    if data is None or not os.path.isfile('/in/'+data):
+        return 'Bad parameters, expected {"file":"<name>"} or "file=<name>"\n', 400
+    xlsx_file = Path('/in/' + data)
+    try:
         wb_obj = openpyxl.load_workbook(xlsx_file)
-        sheet = wb_obj.active
+    except:
+        return f'Bad file {data}, unreadable or wrong format\n', 500
+    sheet = wb_obj.active
 
-        title = True
-        for row in sheet.iter_rows():
-            if title:   # first row is title we skip it
-                title = False
-                continue
+    title = True
+    for row in sheet.iter_rows():
+        if title:  # first row is title we skip it
+            title = False
+            continue
 
-            record = helper.get_one(Rate, product_name=row[0].value)
-            if record is not None:
-                print(f'value {row[0].value} already exists, updating...')
-                record.rate = row[1].value
-                record.scope = row[2].value
-                helper.commit_changes()
-            else:
-                helper.add_instance(Rate, product_name=row[0].value,
-                                    rate=row[1].value,
-                                    scope=row[2].value)
+        record = helper.get_one(Rate, product_name=row[0].value)
+        if record is not None:
+            print(f'value {row[0].value} already exists, updating...')
+            record.rate = row[1].value
+            record.scope = row[2].value
+            helper.commit_changes()
+        else:
+            helper.add_instance(Rate, product_name=row[0].value,
+                                rate=row[1].value,
+                                scope=row[2].value)
 
-        print("hello from POST/rates")
-        return 'OK', 200
+    return 'OK\n', 200

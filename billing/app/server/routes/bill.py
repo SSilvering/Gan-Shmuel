ca80fc4add4e467b80c66cea008a2ce54f6a0d02
shemtov <shemtov.fisher@gmail.com>
2021-04-11 15:53:23 +0300

    fixing bug

--- billing/app/server/routes/bill.py
+++ billing/app/server/routes/bill.py
@@ -13,33 +13,33 @@ bill_blueprint = Blueprint('bill_blueprint', __name__)
 def bill(id):
 	provider = helper.get_one(Provider, id=id)
 	if provider is None:
-		return "no such provider", 400
+		return "wrong provider id", 400
 		
-		now = datetime.now()	
-		from_default = now.strftime('%Y%m') +'01000000'
-		to_default = now.strftime('%Y%m%d%H%M%S')
+	now = datetime.now()	
+	from_default = now.strftime('%Y%m') +'01000000'
+	to_default = now.strftime('%Y%m%d%H%M%S')
 		
-		from_arg = request.args.get('from')
-		if not from_arg:
-			from_arg = from_default
-		else:
-			if len(from_arg) != 14:
-				return "wrong datetime value"
-			try:
-				datetime(from_arg[0:4], from_arg[4:6], from_arg[6:8], from_arg[8:10], from_arg[10:12], from_arg[12:14])
-			except(ValueError):
-				return "wrong datetime value"
-				
-		to_arg = request.args.get('to')
-		if not to_arg:
-			to_arg = to_default
-		else:
-			if len(to_arg) != 14:
-				return "wrong datetime value"
-			try:
-				datetime(to_arg[0:4], to_arg[4:6], to_arg[6:8], to_arg[8:10], to_arg[10:12], to_arg[12:14])
-			except(ValueError):
-				return "wrong datetime value"
+	from_arg = request.args.get('from')
+	if not from_arg:
+		from_arg = from_default
+	else:
+		if len(from_arg) != 14:
+			return "wrong datetime value"
+		try:
+			datetime(from_arg[0:4], from_arg[4:6], from_arg[6:8], from_arg[8:10], from_arg[10:12], from_arg[12:14])
+		except(ValueError):
+			return "wrong datetime value"
+			
+	to_arg = request.args.get('to')
+	if not to_arg:
+		to_arg = to_default
+	else:
+		if len(to_arg) != 14:
+			return "wrong datetime value"
+		try:
+			datetime(to_arg[0:4], to_arg[4:6], to_arg[6:8], to_arg[8:10], to_arg[10:12], to_arg[12:14])
+		except(ValueError):
+			return "wrong datetime value"
 	
 	mocked_json=json.dumps ({
 		"id": provider.id,
@@ -47,7 +47,7 @@ def bill(id):
 		"from": from_arg,
 		"to": to_arg,
 		"truckCount": 0,
-		"sessionCount": ,
+		"sessionCount": 0,
 		"products": [
 			{ "product": "orange",
 				"count": 5, 

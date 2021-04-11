b6b8e8e34d6050edf21f324c13ef965fe5ae26b4
shemtov <shemtov.fisher@gmail.com>
2021-04-11 10:03:54 +0300

    fix truck get

--- billing/app/server/routes/truckGet.py
+++ billing/app/server/routes/truckGet.py
@@ -2,23 +2,26 @@
 from flask import Blueprint, request, jsonify
 from app.server.db.models import *
 from app.server.db.helper import helper
-from datetime import datetime, date
+from datetime import datetime
 from requests.exceptions import HTTPError, ConnectionError
+from json.decoder import JSONDecodeError
 
 # ------- local imports -------
 truckGet_blueprint = Blueprint('truckGet_blueprint', __name__)
 import requests
-from app.setup.settings import WEIGHT_STAGING_URI
+from app.setup.settings import WEIGHT_URI
 
 def get_sessions(truck_id, from_arg, to_arg):
 	try:
-		response = requests.get(WEIGHT_STAGING_URI+f'/item/{truck_id}',
+		response = requests.get(WEIGHT_URI+f'/item/{truck_id}',
 			params={'from': from_arg, 'to': to_arg})
 		response.raise_for_status()
 		return response.json()['sessions']
+	except JSONDecodeError as json_error:
+		print(f'Json responce decoding: {json_error}')	
 	except (HTTPError, ConnectionError) as err:
 		print(f'Bad responce from WEIHGT server: {err}')  
-	return 'mocked get_sessions because bad respone from WEIGHT server'
+	return 'mocked get_sessions because of bad respone from WEIGHT server'
 	
 
 
@@ -28,7 +31,9 @@ def truckGet(id):
 		truck = helper.get_one(Truck, truck_id=id)
 		if truck is None:
 			return 'truck ID not exist', 404
-		from_default = datetime(date.today().year, date.today().month, 1, 00, 00, 00)
+		now = datetime.now()	
+		from_default = now.strftime('%Y%m') +'01000000'
+		to_default = now.strftime('%Y%m%d%H%M%S')
 		
 		from_arg = request.args.get('from')
 		if not from_arg:
@@ -37,18 +42,18 @@ def truckGet(id):
 			if len(from_arg) != 14:
 				return "wrong datetime value"
 			try:
-				from_arg = datetime(from_arg[0:4], from_arg[4:6], from_arg[6:8], from_arg[8:10], from_arg[10:12], from_arg[12:14])
+				datetime(from_arg[0:4], from_arg[4:6], from_arg[6:8], from_arg[8:10], from_arg[10:12], from_arg[12:14])
 			except(ValueError):
 				return "wrong datetime value"
 				
 		to_arg = request.args.get('to')
 		if not to_arg:
-			to_arg = datetime.now()
+			to_arg = to_default
 		else:
 			if len(to_arg) != 14:
 				return "wrong datetime value"
 			try:
-				to_arg = datetime(to_arg[0:4], to_arg[4:6], to_arg[6:8], to_arg[8:10], to_arg[10:12], to_arg[12:14])
+				datetime(to_arg[0:4], to_arg[4:6], to_arg[6:8], to_arg[8:10], to_arg[10:12], to_arg[12:14])
 			except(ValueError):
 				return "wrong datetime value"
 				

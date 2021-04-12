a9a95f1a94c8df6b99edbb7b718aeff8bced8367
fadi <fadi1707@outlook.com>
2021-04-12 11:59:30 +0300

    fixed api session and health

--- weight/weight/weight_app/routes.py
+++ weight/weight/weight_app/routes.py
@@ -23,7 +23,7 @@ def health_check():
         req = requests.get(uri)
         print(req.status_code)
         if req.status_code < 200 or req.status_code > 299:
-            res = f"APP Status is {req.status_code}"
+            res = f"APP api-{item} Status is {req.status_code}"
             return res
     return f"APP status is {req.status_code}"
 

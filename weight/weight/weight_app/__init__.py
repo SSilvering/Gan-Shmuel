8e6c56628401a5444c3d46252dd40edb2f8610e5
Younatan <younatan.huber@gmail.com>
2021-04-12 16:41:55 +0300

    modulise GET/session/<id>

--- weight/weight/weight_app/__init__.py
+++ weight/weight/weight_app/__init__.py
@@ -5,4 +5,4 @@ from mysql.connector import Error
 
 weight_app = Flask(__name__)
 
-from weight_app import routes, GETweight, POSTweight, GETunknown, GETitem, GEThealth
+from weight_app import routes, GETweight, POSTweight, GETunknown, GETitem, GEThealth, GETsession
--- weight/weight/weight_app/routes.py
+++ weight/weight/weight_app/routes.py
@@ -1,14 +1,15 @@
-from weight_app import weight_app, requests, GETweight, POSTweight, GETunknown, GETitem, GEThealth
+from weight_app import weight_app, requests, GETweight, POSTweight, GETunknown, GETitem, GEThealth, GETsession
 from time import gmtime, strftime
 from datetime import datetime
 from flask import request, jsonify,render_template
 import mysql.connector
 from . import weight_app
-# from .POSTweight import POSTweight
+from .POSTweight import POSTweight
 from .GETweight import GETweight
 from .GETitem import GETitem
 from .GEThealth import GEThealth
 from .GETunknown import unknown_func
+from .GETsession import GETsession
 from .db_module import DB_Module
 import json, csv
 
@@ -40,29 +41,7 @@ def check_sesion():
 
 @weight_app.route("/session/<id>")
 def get_session(id="<id>"):
-    if id is not None:
-        select_query = f"SELECT t1.id, t1.trucks_id, t1.bruto, \
-            CASE WHEN t1.direction = 'out' then (select t1.bruto-t1.neto AS 'truckTara' FROM sessions t1, \
-                trucks t2 WHERE t1.id = {id} and t1.trucks_id = t2.truckid ) end tara, t1.neto FROM \
-                sessions t1, trucks t2 WHERE t1.id = {id} and t1.trucks_id = t2.truckid"
-
-        db = DB_Module ()
-        query = f"select direction from sessions where id={id}"
-        data = db.fetch_new_data(query)
-        for item in data:
-            if item == 'out':
-                select_query = f"SELECT t1.id, t1.trucks_id, t1.bruto, t1.bruto-t1.neto AS 'truckTara' t1.neto FROM sessions t1, trucks t2 WHERE t1.id = {id} and t1.trucks_id = t2.truckid"
-            else:
-                select_query = f"SELECT t1.id, t1.trucks_id, t1.bruto FROM sessions t1, \
-                    trucks t2 WHERE t1.id = {id} and t1.trucks_id = t2.truckid"
-        data = db.fetch_new_data(select_query)
-        session = []
-        #for ind in range(0, len(data)):
-        for res in data:
-            session.append(res)
-        return jsonify(session)
-
-    return "provide a truck ID"
+    return GETsession(id)
 
 @weight_app.route('/weight', methods=['GET'])
 def GETweight_route():

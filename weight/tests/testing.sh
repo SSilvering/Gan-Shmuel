e49b9891072a6e3742b2482649a85183741cc027
fadi <fadi1707@outlook.com>
2021-04-13 10:11:28 +0300

    tetsin.sh

--- weight/tests/testing.sh
+++ weight/tests/testing.sh
@@ -5,11 +5,11 @@ IMAG_NAME=$(cat ../.env | grep TAG | cut -d'=' -f2)
 CONTAINER_NAME=$(docker ps | grep $IMAG_NAME | awk '{print $NF}')
 PORT=$(docker inspect $CONTAINER_NAME | grep HostPort | awk -F':' '{print $2}' | cut -d'"' -f2 | uniq)
 ADDRESS=$(docker inspect $CONTAINER_NAME | grep -a4 NetworkID | grep IPAddress | awk -F':' '{print $2}' | cut -d'"' -f2 | uniq | grep -v null)
-APIS="index weight session item batch-weight unknown"
+APIS="index weight session item unknown"
 
 for url in $APIS
 do
-    wget "http://18.194.15.175:8085/$url"
+	curl http://18.194.15.175:8083/$url
 done
 #####
 

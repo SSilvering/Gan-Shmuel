6d374e13ae82bec129302beb15a4959981ddc33d
fadi1707 <81302536+fadi1707@users.noreply.github.com>
2021-04-13 10:20:53 +0300

    Update testing.sh

--- weight/tests/testing.sh
+++ weight/tests/testing.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 
-IMAG_NAME=$(cat ../.env | grep TAG | cut -d'=' -f2)
-CONTAINER_NAME=$(docker ps | grep $IMAG_NAME | awk '{print $NF}')
-PORT=$(docker inspect $CONTAINER_NAME | grep HostPort | awk -F':' '{print $2}' | cut -d'"' -f2 | uniq)
-ADDRESS=$(docker inspect $CONTAINER_NAME | grep -a4 NetworkID | grep IPAddress | awk -F':' '{print $2}' | cut -d'"' -f2 | uniq | grep -v null)
+#IMAG_NAME=$(cat ../.env | grep TAG | cut -d'=' -f2)
+#CONTAINER_NAME=$(docker ps | grep $IMAG_NAME | awk '{print $NF}')
+#PORT=$(docker inspect $CONTAINER_NAME | grep HostPort | awk -F':' '{print $2}' | cut -d'"' -f2 | uniq)
+#ADDRESS=$(docker inspect $CONTAINER_NAME | grep -a4 NetworkID | grep IPAddress | awk -F':' '{print $2}' | cut -d'"' -f2 | uniq | grep -v null)
 APIS="index weight session item unknown"
 
 for url in $APIS
